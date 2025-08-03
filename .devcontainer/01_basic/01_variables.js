ac6353b37be3ceb96651dbc9d5848cf9cf9dc6af
rahul-1234168 <rahul.23651@knit.ac.in>
2025-08-03 05:40:26 +0000

    variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail ="example@example.com"
+var accountPassword ="password123"
+accountCity="newyork"
+let accountState;
+
+//accountId =2584
+accountEmail="hc@hc.com"
+accountPassword="newpassword456"
+accountCity="sanfrancisco"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*
+  prefer not to use var,
+  becz of block scope and function scope issues.
+*/
+//console.log(accountId);
+
